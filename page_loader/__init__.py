63ab473451cc8ca699fb011237fe4cca6cb38d08
ilyas <ilyasers@gmail.com>
2022-02-09 14:07:40 +0300

    feat: add download function

---
+++ page_loader/__init__.py
@@ -0,0 +1,25 @@
+import os
+from urllib.parse import urlparse
+import requests
+
+
+def url_to_filename(url):
+    parsed_url = urlparse(url)
+    schema = parsed_url.scheme + '://'
+    url_without_schema = url.replace(schema, '')
+
+    filename = url_without_schema \
+        .replace('/', '-') \
+        .replace('.', '-')
+
+    return filename
+
+
+def download(url, dir=os.getcwd()):
+    filename = url_to_filename(url)
+    filepath = os.path.join(dir, filename)
+
+    res = requests.get(url)
+
+    with open(filepath, 'w') as f:
+        f.write(res.text)
---
+++ tests/test_page_loader.py
@@ -0,0 +1,27 @@
+import os
+import tempfile
+
+from page_loader import download
+
+
+def get_fixture_path(name):
+    return os.path.join('tests/fixtures', name)
+
+
+def test_page_loader(requests_mock):
+    expected_path = get_fixture_path('expected.html')
+
+    with tempfile.TemporaryDirectory() as tmpdirname, open(expected_path) as expected_file:
+        url = 'https://test.com/file'
+        filename = 'test-com-file'
+        expected = expected_file.read()
+
+        requests_mock.get(url, text=expected)
+
+        download(url, tmpdirname)
+
+        result_path = os.path.join(tmpdirname, filename)
+
+        with open(result_path) as result_file:
+            actual = result_file.read()
+            assert expected == actual
