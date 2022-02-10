68b6bef5bff02235f02f1342384b15d2fddd2cec
ilyas <ilyasers@gmail.com>
2022-02-10 10:39:18 +0300

    feat: implement images loading

--- page_loader/__init__.py
+++ page_loader/__init__.py
@@ -1,6 +1,11 @@
 import os
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 import requests
+from bs4 import BeautifulSoup
+
+
+def parse(file_or_content):
+    return BeautifulSoup(file_or_content, 'html.parser')
 
 
 def url_to_filename(url):
@@ -15,13 +20,44 @@ def url_to_filename(url):
     return filename
 
 
+def save_binary(url, path):
+    res = requests.get(url, stream=True)
+    with open(path, 'wb') as f:
+        for chunk in res.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+
 def download(url, dir):
-    filename = url_to_filename(url) + '.html'
+    basename = url_to_filename(url)
+    base_url, _ = os.path.split(url)
+    filename = basename + '.html'
     filepath = os.path.join(dir, filename)
 
     res = requests.get(url)
+    content = res.text
+    soup = parse(content)
+    images = soup.find_all("img")
+    files_dirname = f'{basename}_files'
+    files_dirpath = os.path.join(dir, files_dirname)
+
+    if len(images) > 0:
+        os.mkdir(files_dirpath)
+
+    for img in images:
+        old_src = img['src']
+        img_url = urlparse(old_src)
+        _, filename = os.path.split(img_url.path)
+        new_src = os.path.join(files_dirname, filename)
+        img['src'] = new_src
+
+        img_path = os.path.join(dir, new_src)
+        save_binary(
+            url=urljoin(base_url, old_src),
+            path=img_path
+        )
 
     with open(filepath, 'w') as f:
-        f.write(res.text)
+        f.write(soup.prettify())
 
     return filepath
