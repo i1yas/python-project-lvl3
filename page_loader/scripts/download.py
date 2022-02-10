e127d0cd50dd34338bf5f7c29f32e42d4b387c9d
ilyas <ilyasers@gmail.com>
2022-02-10 18:13:53 +0300

    feat: add error handling in cli script

--- page_loader/scripts/download.py
+++ page_loader/scripts/download.py
@@ -1,7 +1,8 @@
 import os
+import sys
 import argparse
 
-from page_loader import download
+from page_loader import download, get_default_logger, HTTPResponseException
 
 
 def main():
@@ -12,11 +13,31 @@ def main():
                         help='location for downloaded file')
 
     args = parser.parse_args()
-    filepath = download(
-        args.url,
-        args.OUTPUT or os.getcwd()
-    )
-    print(filepath)
+
+    logger = get_default_logger()
+
+    try:
+        filepath = download(
+            args.url,
+            dir=args.OUTPUT or os.getcwd(),
+            logger=logger
+        )
+        print(filepath)
+        sys.exit(0)
+    except HTTPResponseException as e:
+        message = f'Reponse error. {e.message}'
+        print(message)
+    except FileNotFoundError as e:
+        message = f'File or directory {e.filename} not found on the disk'
+        logger.error(message)
+    except FileExistsError as e:
+        message = f'File or directory {e.filename} already exists'
+        logger.error(message)
+    except PermissionError:
+        message = f'Can\'t access {args.OUTPUT}'
+        logger.error(message)
+
+    sys.exit(20)
 
 
 if __name__ == '__main__':
